e13b7ab3e75dcb551f9a03d48f91521d023561a3
Paul Huynh <paulhuynh94@gmail.com>
2017-06-24 19:26:08 -0700

    Some middleware stuff

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const request = require('request')
+const axios = require('axios')
 const app = express()
 
 app.use('/static',express.static('public'));
@@ -7,6 +9,46 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 });
 
+app.post('/sendlink', function(req,res){
+	console.log("LOOK");
+	
+	axios({
+		method: 'post',
+		url: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?',
+		headers:{
+			'Content-Type' : 'application/json',
+			'Ocp-Apim-Subscription-Key' : '7148e51451224d669d207afd909c9b75'
+			
+		},
+		data: '{"url": "https://shop.nationalgeographic.com/html/catalog/photogalleries/reza/images/reza8.jpg"}'
+	}).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.log(error);
+    });
+
+	/*
+	request.post({
+		'headers':{
+			'Content-Type' : 'application/json',
+			'Ocp-Apim-Subscription-Key' : '7148e51451224d669d207afd909c9b75'
+			
+		},
+		url: 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize?',
+		'data':{
+			'url' : 'https://shop.nationalgeographic.com/html/catalog/photogalleries/reza/images/reza8.jpg',
+		},		
+		function(error,response,body){
+			console.log(response.body);
+			
+		}
+	})
+	*/
+		
+	
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 3000!')
 })
+
